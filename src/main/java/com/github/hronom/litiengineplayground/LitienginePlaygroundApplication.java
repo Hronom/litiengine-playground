2490357ce86c361ca2210657c3901c37dd74ae7d
Hronom <hronom@gmail.com>
2021-08-29 17:35:56 +0300

    Initial commit

---
+++ src/main/java/com/github/hronom/litiengineplayground/LitienginePlaygroundApplication.java
@@ -0,0 +1,11 @@
+package com.github.hronom.litiengineplayground;
+
+import de.gurkenlabs.litiengine.Game;
+
+public class LitienginePlaygroundApplication {
+
+	public static void main(String[] args) {
+		Game.init(args);
+		Game.start();
+	}
+}
---
+++ src/test/java/com/github/hronom/litiengineplayground/LitienginePlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hronom.litiengineplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LitienginePlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
